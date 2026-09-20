33fe1d8cc94cb101f51dd6b3e046ff891fae417b
Hitech-Global <hitechjames@163.com>
2026-09-20 09:17:07 +0800

    test(shopee): cover structural action gates

--- shopee-analytics/test/action-engine.test.cjs
+++ shopee-analytics/test/action-engine.test.cjs
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const { itemAction, campaignActions } = require('../src/action-engine');
+const { itemAction, campaignActions, structuralActionGates } = require('../src/action-engine');
 
 const item = itemAction('PRODUCT_OPTIMIZATION_CANDIDATE');
 assert.strictEqual(item.code, 'PRODUCT_TEST_FIRST');
@@ -18,8 +18,8 @@ const lowVolumeEfficient = campaignActions({
     { state: 'EXPLORATION_KEEP' },
   ],
 });
-assert(lowVolumeEfficient.some(x => x.code === 'THREE_DAY_CORE_VOUCHER'));
-assert(!lowVolumeEfficient.some(x => x.code === 'PROTECT_PROFIT'));
+assert(lowVolumeEfficient.recommendations.some(x => x.code === 'THREE_DAY_CORE_VOUCHER'));
+assert(!lowVolumeEfficient.recommendations.some(x => x.code === 'PROTECT_PROFIT'));
 
 const overSpend = campaignActions({
   campaign: {
@@ -29,8 +29,8 @@ const overSpend = campaignActions({
   },
   items: [{ state: 'HIGH_RISK_ZERO_ORDER' }],
 });
-assert.strictEqual(overSpend[0].code, 'PROTECT_PROFIT');
-assert(overSpend.some(x => x.code === 'CONCENTRATE_BUDGET'));
+assert.strictEqual(overSpend.recommendations[0].code, 'PROTECT_PROFIT');
+assert(overSpend.recommendations.some(x => x.code === 'CONCENTRATE_BUDGET'));
 
 console.log('shopee action engine tests: ok');
 
@@ -47,3 +47,25 @@ const scalableCore = itemAction('CORE_CANDIDATE', {
   scaleEligible: true,
 });
 assert.strictEqual(scalableCore.code, 'CONTROLLED_SINGLE_ITEM_SCALE');
+
+const learningGates = structuralActionGates({
+  campaign: { maturityStatus: 'LEARNING', roasState: 'TARGET_MET', spendLimitState: 'WITHIN_SPEND_LIMIT', budgetUtilization: 1 },
+  items: [{ state: 'HIGH_RISK_ZERO_ORDER', scaleEligibility: { eligible: false } }],
+});
+assert.strictEqual(learningGates.REMOVE_SKU.allowed, false);
+assert.strictEqual(learningGates.INCREASE_BUDGET.allowed, false);
+
+const stableScaleGates = structuralActionGates({
+  campaign: { maturityStatus: 'STABLE', roasState: 'TARGET_MET', spendLimitState: 'WITHIN_SPEND_LIMIT', budgetUtilization: 0.95 },
+  items: [{ state: 'CORE_CANDIDATE', scaleEligibility: { eligible: true } }],
+});
+assert.strictEqual(stableScaleGates.SPLIT_SINGLE_ITEM.allowed, true);
+assert.strictEqual(stableScaleGates.INCREASE_BUDGET.allowed, true);
+assert.strictEqual(stableScaleGates.LOWER_TARGET_ROAS.allowed, false);
+
+const underSpendGates = structuralActionGates({
+  campaign: { maturityStatus: 'STABLE', roasState: 'TARGET_MET', spendLimitState: 'WITHIN_SPEND_LIMIT', budgetUtilization: 0.55 },
+  items: [{ state: 'CORE_CANDIDATE', scaleEligibility: { eligible: true } }],
+});
+assert.strictEqual(underSpendGates.LOWER_TARGET_ROAS.allowed, true);
+assert.strictEqual(underSpendGates.INCREASE_BUDGET.allowed, false);
